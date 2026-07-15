d8a9897ba823e240271cadac194fd1e2fbafe308
rightokon3 <rightokon3@gmail.com>
2026-07-16 00:37:08 +0100

    added .env

--- config/firebase.ts
+++ config/firebase.ts
@@ -6,13 +6,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '', 
-  messagingSenderId: '',
-  appId: '',
+  apiKey: process.env.EXPO_PUBLIC_CLOUDINARY_CLOUD_NAME!,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL!,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID!,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
