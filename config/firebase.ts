9c49350da1b359abbfbcc922ca5edd984bfba8cc
rightokon3 <rightokon3@gmail.com>
2026-07-18 04:44:07 +0100

    fixed the login and sign up issue plus added the missing tailwind file

--- config/firebase.ts
+++ config/firebase.ts
@@ -6,7 +6,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_CLOUDINARY_CLOUD_NAME!,
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY!,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN!,
   databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL!,
   projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID!,
